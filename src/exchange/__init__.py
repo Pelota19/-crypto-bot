2d3f10cc30419a02cf6456b7da467e91e55bb680
Pelota19 <59800973+Pelota19@users.noreply.github.com>
2025-09-03 18:00:16 -0300

    Refactor exchange module and add docstring
    Removed BinanceClient class initialization code and added module docstring.

--- src/exchange/__init__.py
+++ src/exchange/__init__.py
@@ -1,19 +1,4 @@
-# Exchaclass BinanceClient:
-    def __init__(self, api_key: str = API_KEY, api_secret: str = API_SECRET, use_testnet: bool = USE_TESTNET, dry_run: bool = DRY_RUN):
-        self.dry_run = dry_run
-        opts = {'defaultType': 'future'}
-        self.exchange = ccxt.binance({
-            'apiKey': api_key,
-            'secret': api_secret,
-            'enableRateLimit': True,
-            'options': opts,
-        })
-
-        if use_testnet:
-            # Testnet oficial Binance Futures
-            self.exchange.options['defaultType'] = 'future'
-            self.exchange.urls['api'] = {
-                'public': 'https://testnet.binancefuture.com/fapi/v1',
-                'private': 'https://testnet.binancefuture.com/fapi/v1'
-            }
-            logger.info("Binance testnet mode enabled")nge module
+# src/exchange/__init__.py
+"""
+Paquete exchange: conecta con exchanges soportados.
+"""
